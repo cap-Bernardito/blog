5ff4ba44a84fa51134cc32fb21689cfe3bbff878
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2023-09-15 15:19:36 +0300

    feat: добавляет переключение тем

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,20 +1,29 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { Suspense } from "react";
+import cn from "classnames";
 import { AboutPage } from "pages/about-page";
 import { HomePage } from "pages/home-page";
-import "./styles/style.scss";
+import { useTheme } from "shared/lib/toglle_theme";
+import "./styles/index.scss";
 
-export const App = () => (
-  <div className="app">
-    <Link to="/">Главная</Link>
-    <Link to="/about">О нас</Link>
-    <hr />
-    <br />
-    <Suspense fallback={<>...Loading</>}>
-      <Routes>
-        <Route path="about" element={<AboutPage />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Suspense>
-  </div>
-);
+export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={cn("app", `app-${theme}`)}>
+      <button onClick={toggleTheme}>Сменить тему</button>
+      <br />
+      <br />
+      <Link to="/">Главная</Link>
+      <Link to="/about">О нас</Link>
+      <hr />
+      <br />
+      <Suspense fallback={<>...Loading</>}>
+        <Routes>
+          <Route path="about" element={<AboutPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom/client";
-import { App } from "./app/app";
-
 import { BrowserRouter } from "react-router-dom";
+import { App } from "app/app";
+import { ThemeProvider } from "shared/lib/toglle_theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement);
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
 );
