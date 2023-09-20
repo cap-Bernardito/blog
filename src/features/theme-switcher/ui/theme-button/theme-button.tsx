760891f284c742f3a91f22c9d2459d6c6fb795ad
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2023-09-20 15:46:27 +0300

    chore: добавляет stylelint

--- src/features/theme-switcher/ui/theme-button/theme-button.tsx
+++ src/features/theme-switcher/ui/theme-button/theme-button.tsx
@@ -5,7 +5,6 @@ import { useTheme } from "../../model/useTheme";
 import { Theme } from "../../model/theme-context";
 import LightIcon from "shared/assets/icons/theme-light.svg";
 import DarkIcon from "shared/assets/icons/theme-dark.svg";
-import css from "./theme-button.module.scss";
 
 type ThemeButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   className?: string;
@@ -15,7 +14,7 @@ export const ThemeButton: React.FC<ThemeButtonProps> = ({ className }) => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <Button className={cn(css.root, className, theme)} variant={ButtonVariant.CLEAR} onClick={toggleTheme}>
+    <Button className={cn(className, theme)} variant={ButtonVariant.CLEAR} onClick={toggleTheme}>
       {theme === Theme.DARK ? <LightIcon /> : <DarkIcon />}
     </Button>
   );
