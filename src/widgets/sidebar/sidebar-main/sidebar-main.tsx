9a8270367750672b94c73106998568b3d495b061
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2023-09-22 18:01:23 +0300

    feat: стилизует кнопку переключения сайдбара

--- src/widgets/sidebar/sidebar-main/sidebar-main.tsx
+++ src/widgets/sidebar/sidebar-main/sidebar-main.tsx
@@ -3,6 +3,8 @@ import cn from "classnames";
 import { useTranslation } from "react-i18next";
 import { ThemeButton } from "features/theme-switcher";
 import { LangSwitcher } from "features/lang-switcher";
+import { Button, ButtonVariant } from "shared/ui/button";
+import ArrowBottom from "shared/assets/icons/arrow-bottom.svg";
 import css from "./sidebar-main.module.scss";
 
 type SidebarMainProps = {
@@ -18,9 +20,14 @@ export const SidebarMain: React.FC<SidebarMainProps> = ({ className }) => {
       <div className={cn(css.root__main)}>
         {t("Сайдбар")}
         <br />
-        <button data-testid="sidebar-toggle" onClick={() => setCollapsed((state) => !state)}>
-          {t("Сжать сайдбар")}
-        </button>
+        <Button
+          className={css.root__toggle}
+          data-testid="sidebar-toggle"
+          variant={ButtonVariant.CLEAR}
+          onClick={() => setCollapsed((state) => !state)}
+        >
+          <ArrowBottom className={css.root__toggle_icon} width={32} height={32} viewBox="0 0 32 32" />
+        </Button>
       </div>
       <div className={cn(css.root__switchers)}>
         <ThemeButton />
