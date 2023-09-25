d385827c09e54169c6acb81dd499313c57d6c32a
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2023-09-25 12:47:58 +0300

    test: исправляет тесты для компонентов с роутами

--- src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import { render } from "@testing-library/react";
 import { I18nextProvider } from "react-i18next";
 import i18nForTests from "shared/config/i18n/i18nForTests";
 
-export function renderWithTranslation(component: React.ReactNode) {
-  return render(<I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>);
+export function renderWithRouterAndTranslation(component: React.ReactNode) {
+  return render(
+    <BrowserRouter>
+      <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
+    </BrowserRouter>,
+  );
 }
--- src/widgets/sidebar/sidebar-main/sidebar-main.test.tsx
+++ src/widgets/sidebar/sidebar-main/sidebar-main.test.tsx
@@ -1,16 +1,16 @@
 import { fireEvent, screen } from "@testing-library/react";
 import { SidebarMain as Sidebar } from "./sidebar-main";
-import { renderWithTranslation } from "shared/lib/tests/renderWithTranslation/renderWithTranslation";
+import { renderWithRouterAndTranslation } from "shared/lib/tests/renderWithTranslation/renderWithTranslation";
 
 describe("Sidebar", () => {
   it("should be defined", () => {
-    renderWithTranslation(<Sidebar />);
+    renderWithRouterAndTranslation(<Sidebar />);
 
     expect(screen.getByTestId("sidebar")).toBeDefined();
   });
 
   it("should contain a toggle button", () => {
-    renderWithTranslation(<Sidebar />);
+    renderWithRouterAndTranslation(<Sidebar />);
 
     const toggleBtn = screen.getByTestId("sidebar-toggle");
     fireEvent.click(toggleBtn);
@@ -19,7 +19,7 @@ describe("Sidebar", () => {
   });
 
   it("should switch", () => {
-    renderWithTranslation(<Sidebar />);
+    renderWithRouterAndTranslation(<Sidebar />);
 
     const toggleBtn = screen.getByTestId("sidebar-toggle");
     fireEvent.click(toggleBtn);
