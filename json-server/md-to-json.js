faff8f24b39ee739fc5c6506fca759112b80be0a
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2023-10-19 13:41:35 +0300

    chore(server): дорабатывает генератор статей из markdown
    Было: код страницы поставлялся в виде строки c html
    Стало:  код страницы поставляется в виде массива объектов для последующей генерации react элементов на клиенте

--- json-server/md-to-json.js
+++ json-server/md-to-json.js
@@ -1,14 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fsPromises = require("fs").promises;
 const fm = require("front-matter");
-const MarkdownIt = require("markdown-it");
 const prettier = require("prettier");
 const fg = require("fast-glob");
+const parseMarkdown = require("./parse-markdown");
 
 const FROM = [`articles/*.md`];
 const TO = `${__dirname}/db-parts/articles.js`;
-
-const md = new MarkdownIt();
 
 const processFiles = async () => {
   const stream = fg.globStream(FROM, { cwd: __dirname, absolute: true });
@@ -20,9 +18,9 @@ const processFiles = async () => {
 
   const result = (await Promise.all(tasks)).map((file) => {
     const { attributes, body } = fm(file);
-    const html = md.render(body);
+    const { elements } = parseMarkdown(body);
 
-    return { ...attributes, body: html };
+    return { ...attributes, body: elements };
   });
 
   const resultStringify = await prettier.format(`module.exports = ${JSON.stringify(result)};`, { parser: "babel" });
