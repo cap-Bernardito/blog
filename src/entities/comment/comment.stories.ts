6d8d9db72a5224a72d01fb9df3c67ae7bae91bcb
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2023-10-23 14:10:04 +0300

    feat: добавляет entities/comment

---
+++ src/entities/comment/comment.stories.ts
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Theme } from "entities/theme";
+
+import { Comment } from "./comment";
+
+const meta = {
+  title: "entities/Comment",
+  component: Comment,
+  parameters: {
+    layout: "padded",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Comment>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const DefaultLoading: Story = {
+  args: {},
+};
+
+export const DefaultDarkLoading: Story = {
+  args: {},
+  parameters: { theme: Theme.DARK },
+};
+
+export const Default: Story = {
+  args: {
+    img: "tigger.jpg",
+    title: "Вася Василёк",
+    children: `Программа, которую по традиции называют «Hello, world!», очень проста. Она выводит куда-либо фразу «Hello, world!», или другую подобную, средствами некоего языка.
+    JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.
+    `,
+  },
+};
+
+export const DefaultDark: Story = {
+  args: {
+    img: "tigger.jpg",
+    title: "Вася Василёк",
+    children: `Программа, которую по традиции называют «Hello, world!», очень проста. Она выводит куда-либо фразу «Hello, world!», или другую подобную, средствами некоего языка.
+    JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.
+    `,
+  },
+  parameters: { theme: Theme.DARK },
+};
---
+++ src/entities/comment/comment.tsx
@@ -0,0 +1,27 @@
+import cn from "classnames";
+import React from "react";
+
+import { Avatar } from "shared/ui/avatar";
+import { Skeleton } from "shared/ui/skeleton";
+
+import css from "./comment.module.scss";
+
+type CommentProps = {
+  className?: string;
+  img?: string;
+  title?: string;
+};
+
+export const Comment: React.FC<React.PropsWithChildren<CommentProps>> = ({ className, img, title, children }) => {
+  return (
+    <div className={cn(css.root, className)}>
+      <div className={cn(css.root__left)}>
+        <Avatar url={img} />
+      </div>
+      <div className={cn(css.root__right)}>
+        <div className={cn(css.title)}>{title || <Skeleton count={1} height={20} style={{maxWidth: "300px"}} />}</div>
+        <div className={cn(css.body)}>{children || <Skeleton count={5} height={18} />}</div>
+      </div>
+    </div>
+  );
+};
