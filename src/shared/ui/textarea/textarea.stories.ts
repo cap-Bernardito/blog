45e6dce2efcdc64ff9bdf82428623641f87cd267
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2023-10-24 20:45:04 +0300

    feat: добавляет textarea component

---
+++ src/shared/ui/textarea/textarea.stories.ts
@@ -0,0 +1,90 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Theme } from "entities/theme";
+
+import { Textarea } from "./textarea";
+
+const meta = {
+  title: "shared/Textarea",
+  component: Textarea,
+  parameters: {
+    layout: "padded",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Textarea>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {
+  args: {
+    value: "",
+    label: "Введите сообщение",
+    placeholder: "Введите сообщение",
+  },
+};
+
+export const Dark: Story = {
+  args: {
+    value: "",
+    label: "Введите сообщение",
+    placeholder: "Введите сообщение",
+  },
+  parameters: { theme: Theme.DARK },
+};
+
+export const LightWithValue: Story = {
+  args: {
+    value: "Сообщение",
+    label: "Введите сообщение",
+    placeholder: "Введите сообщение",
+  },
+};
+
+export const DarkWithValue: Story = {
+  args: {
+    value: "Сообщение",
+    label: "Введите сообщение",
+    placeholder: "Введите сообщение",
+  },
+  parameters: { theme: Theme.DARK },
+};
+
+export const LightWithLongValue: Story = {
+  args: {
+    value:
+      "JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы",
+    label: "Введите сообщение",
+    placeholder: "Введите сообщение",
+  },
+};
+
+export const DarkWithLongValue: Story = {
+  args: {
+    value:
+      "JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы",
+    label: "Введите сообщение",
+    placeholder: "Введите сообщение",
+  },
+  parameters: { theme: Theme.DARK },
+};
+
+export const LightWithError: Story = {
+  args: {
+    value: "",
+    error: "Поле не может быть пустым",
+    label: "Введите сообщение",
+    placeholder: "Введите сообщение",
+  },
+};
+
+export const DarkWithError: Story = {
+  args: {
+    value: "",
+    error: "Поле не может быть пустым",
+    label: "Введите сообщение",
+    placeholder: "Введите сообщение",
+  },
+  parameters: { theme: Theme.DARK },
+};
---
+++ src/shared/ui/textarea/textarea.tsx
@@ -0,0 +1,71 @@
+import cn from "classnames";
+import React, { TextareaHTMLAttributes, useEffect, useRef, useState } from "react";
+
+import css from "./textarea.module.scss";
+
+type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement> & {
+  className?: string;
+  label: string;
+  value?: string;
+  error?: string;
+  onChange?: React.ChangeEventHandler;
+};
+
+export const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>((props, ref) => {
+  const { className, label, id, error, rows = 1, placeholder, value, ...otherProps } = props;
+  const fieldHint = `${otherProps.name}-error-info`;
+  const [rowsCount, setRowCount] = useState<number>(rows);
+  const taRef = useRef<HTMLTextAreaElement | null>(null);
+  const divRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const field = taRef.current;
+    const div = divRef.current;
+
+    if (field && div) {
+      const fieldHeight = div.scrollHeight;
+      const lineHeight = Number.parseFloat(getComputedStyle(div).lineHeight);
+      const estimatedRows = Math.ceil(fieldHeight / lineHeight - 1);
+
+      setRowCount(estimatedRows > 7 ? 7 : Math.max(estimatedRows, 1));
+    }
+  }, [ref, value]);
+
+  return (
+    <div className={cn(css.root, className)}>
+      <div className={cn(css.root__sizer)} ref={divRef} role="none">
+        {value}
+      </div>
+      <textarea
+        className={css.root__field}
+        id={id}
+        rows={rowsCount}
+        ref={(node) => {
+          taRef.current = node;
+
+          if (typeof ref === "function") {
+            ref(node);
+          } else if (ref) {
+            ref.current = node;
+          }
+        }}
+        placeholder={placeholder}
+        value={value}
+        {...otherProps}
+        aria-describedby={error ? fieldHint : undefined}
+      />
+      <label className={"sr-only"} htmlFor={id}>
+        {label}
+      </label>
+      {error && (
+        <div className={cn(css.root__error, css["error-active"])}>
+          <div className={cn(css.root__error_inner)} id={fieldHint}>
+            {error}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+});
+
+Textarea.displayName = "Textarea";
