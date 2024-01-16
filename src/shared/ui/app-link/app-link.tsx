3a00ede9492d222ddbdea574d5376d810615d3db
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2024-01-16 10:10:32 +0300

    feat: подсвечивает активный пункт меню

--- src/shared/ui/app-link/app-link.tsx
+++ src/shared/ui/app-link/app-link.tsx
@@ -1,6 +1,6 @@
 import cn from "classnames";
 import React from "react";
-import { Link, LinkProps } from "react-router-dom";
+import { LinkProps, NavLink } from "react-router-dom";
 
 import css from "./app-link.module.scss";
 
@@ -19,8 +19,12 @@ export const AppLink: React.FC<React.PropsWithChildren<AppLinkProps>> = (props) 
   const { to, className, children, color = AppLinkColor.DEFAULT, ...otherProps } = props;
 
   return (
-    <Link className={cn(css.root, className, css[color])} to={to} {...otherProps}>
+    <NavLink
+      className={({ isActive }) => cn(css.root, className, css[color], isActive && css.active)}
+      to={to}
+      {...otherProps}
+    >
       {children}
-    </Link>
+    </NavLink>
   );
 };
