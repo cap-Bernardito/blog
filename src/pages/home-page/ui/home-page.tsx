1d594eb3a1574607eaeed68ae47fb55830546c26
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2024-01-25 11:59:45 +0300

    refactor: home page

--- src/pages/home-page/ui/home-page.tsx
+++ src/pages/home-page/ui/home-page.tsx
@@ -1,40 +1,42 @@
 import { useTranslation } from "react-i18next";
 
-import { useAppSelector } from "app/app-store";
-
 import { OpenLoginModal } from "widgets/login-modal";
 
 import { LoginButtonText } from "entities/login-button";
-import { sessionSelectors } from "entities/session";
+import { withAuth } from "entities/session";
 
 import "shared/config/i18n/i18n";
 
+const UserGreeting = withAuth({
+  Authorized: ({ viewer }) => <p> Привет, {viewer.first} </p>,
+  UnAuthorized: () => (
+    <>
+      <p>
+        Для просмотра материалов{" "}
+        <OpenLoginModal
+          LoginButton={({ ...props }) => (
+            <LoginButtonText variant="clear" color="secondary" {...props}>
+              авторизируйтесь
+            </LoginButtonText>
+          )}
+        />{" "}
+        на сайте. <br />
+      </p>
+      <p>Демо доступ:</p>
+      <blockquote>
+        <b>Login:</b> admin / user; <br /> <b>Password:</b> 123
+      </blockquote>
+    </>
+  ),
+});
+
 export const HomePage = () => {
-  const isAuth = useAppSelector(sessionSelectors.isAuth);
   const { t } = useTranslation("home");
 
   return (
     <div>
       {t("Главная страница")}
-      {isAuth ? null : (
-        <>
-          <p>
-            Для просмотра материалов{" "}
-            <OpenLoginModal
-              LoginButton={({ ...props }) => (
-                <LoginButtonText variant="clear" color="secondary" {...props}>
-                  авторизируйтесь
-                </LoginButtonText>
-              )}
-            />{" "}
-            на сайте. <br />
-          </p>
-          <p>Демо доступ:</p>
-          <blockquote>
-            <b>Login:</b> admin / user; <br /> <b>Password:</b> 123
-          </blockquote>
-        </>
-      )}
+      <UserGreeting />
     </div>
   );
 };
